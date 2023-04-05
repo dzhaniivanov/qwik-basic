0a7e1e3e43e5354b83e287810f8feee7b5f08e69
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2023-04-05 09:37:23 +0300

    basic routing

---
+++ src/routes/about/index.tsx
@@ -0,0 +1,13 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+    console.log('hello')
+    return (
+        <article>
+            <h2>About</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt consectetur quidem impedit voluptate nostrum ex, quis aliquid. Eos, cumque facere!</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt consectetur quidem impedit voluptate nostrum ex, quis aliquid. Eos, cumque facere!</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt consectetur quidem impedit voluptate nostrum ex, quis aliquid. Eos, cumque facere!</p>
+        </article>
+    )
+})
---
+++ src/routes/contact/index.tsx
@@ -0,0 +1,10 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+    return (
+        <article>
+            <h2>Contact</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt consectetur quidem impedit voluptate nostrum ex, quis aliquid. Eos, cumque facere!</p>
+        </article>
+    )
+})
